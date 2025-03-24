7c4a7effad5331237d68dfcd9642ed3e3def35b2
MEET2231 <60254693+MEET2231@users.noreply.github.com>
2025-03-24 22:43:01 +0530

    Update Reports.jsx

--- src/pages/Reports.jsx
+++ src/pages/Reports.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/reports";
+const API_URL = "https://redalert-backend-jxzv.onrender.com/api/reports";
 
 const Reports = () => {
   const [reports, setReports] = useState([]);
